6ddb2dbb9f4e15c682d5a819beb34e0ed92d6ad7
Aviral Tandon <aviraltandon20@gmail.com>
2021-04-23 21:39:10 +0530

    Fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+const head = {
+  position: 'sticky',
+  top: '0',
+  backgroundColor: 'rgb(18,18,18)',
+  padding: '10px 0'
+
+};
+
 const Header = () => {
   return (
-    <nav className='space-x-10'>
+    <nav className='space-x-10' style={head}>
       <ul className='flex flex-row space-x-4 md:space-x-10 md:text-xl justify-center md:justify-end font-thin'>
         <li>
           <a href='#projects' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
