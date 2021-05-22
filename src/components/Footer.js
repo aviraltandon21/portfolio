8612cb360202256dd3e93856b58d8957485dc557
Aviral Tandon <aviraltandon20@gmail.com>
2021-05-22 12:55:12 +0530

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-        <h3 className="footer__head">Designed and Devloped by Aviral Tandon</h3>
+        <h5 className="footer__head">Designed and Devloped by Aviral Tandon</h5>
         </div>
     )
 }
