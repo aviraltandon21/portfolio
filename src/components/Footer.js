4e12fe045aacd3506cb0c43b67dcfc5228f723b9
Aviral Tandon <201951036@iiitvadodara.ac.in>
2021-05-23 22:30:50 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-        <h4 className="footer__head">Designed and Devloped by Aviral Tandon</h4>
+        <h4 className="footer__head">Designed and Developed by Aviral Tandon</h4>
         </div>
     )
 }
