92fd689229cf91e104728e4ccdb3b7ff9574c611
Aviral Tandon <201951036@iiitvadodara.ac.in>
2021-05-25 00:30:50 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,7 +63,7 @@ const Contact = () => {
         </div>
 
         <div className='bg-gray px-5 py-10 md:py-8 sm:p-8 my-2 md:rounded-lg shadow-lg  justify-between w-full  md:w-6/12 '>
-          <form className='flex flex-col space-y-3 m-auto w-full' name="contact" method="POST" data-netlify="true">
+          <form className='flex flex-col space-y-3 m-auto w-full' name="contact" method="POST" netlify>
             
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id='name' className='gradient pl-1 pr-1' required></input>
